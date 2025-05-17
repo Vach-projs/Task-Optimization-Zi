d4649b9986300757367d47681952f364beec01c5
Vach-projs <vachanaucsecsec@gmail.com>
2025-05-17 17:01:30 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import SentimentPrediction, HRNotification, Team
-
-# Register models
-admin.site.register(SentimentPrediction)
-admin.site.register(HRNotification)
-admin.site.register(Team)
-
